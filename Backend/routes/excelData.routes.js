ac96e564e5245d2caeb6c474c5b49328bac3e900
ras23244 <146091681+ras23244@users.noreply.github.com>
2025-06-12 02:43:20 -0700

    Update excelData.routes.js

--- Backend/routes/excelData.routes.js
+++ Backend/routes/excelData.routes.js
@@ -1,28 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 const multer = require("multer");
 const excelDataController = require("../controllers/excelData.controllers");
 const authMiddleware = require("../middlewares/auth.middleware");
 
-// Disk storage setup
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "..", "uploads")); // uploads folder
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  },
-});
-
+// Use memory storage instead of disk storage
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Route to upload Excel file
 router.post(
   "/upload",
   authMiddleware.authUser,
-  upload.single("file"), // file field name in formData
+  upload.single("file"),
   excelDataController.uploadExcelData
 );
 
